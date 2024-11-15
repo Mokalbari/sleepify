8b5957756aef3ae786507116738bcc7ecc0c2014
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-11-15 19:29:54 +0100

    add props to track card template

--- src/components/track-card.tsx
+++ src/components/track-card.tsx
@@ -2,13 +2,22 @@ import Image from "next/image"
 import cover from "../../public/cover-album_placeholder.webp"
 import HeartButton from "./ui/heart-button"
 
-export default function TrackCard() {
+interface Props {
+  trackName: string
+  artistName: string
+  trackImage: string
+}
+export default function TrackCard({
+  trackName,
+  artistName,
+  trackImage,
+}: Props) {
   return (
-    <div className="brutal flex items-center justify-between rounded-md bg-white px-6 py-3 sm:py-4">
+    <li className="brutal flex items-center justify-between rounded-md bg-white px-6 py-3 sm:py-4">
       <div className="flex items-center">
         <div className="relative w-12 sm:w-16">
           <Image
-            src={cover}
+            src={trackImage || cover}
             alt="album cover"
             width={777}
             height={777}
@@ -16,11 +25,11 @@ export default function TrackCard() {
           />
         </div>
         <div className="ml-4">
-          <h3 className="font-bold sm:text-sm">Common Freestyle</h3>
-          <div className="text-xs sm:text-sm">RH Factor</div>
+          <h3 className="font-bold sm:text-sm">{trackName}</h3>
+          <div className="text-xs sm:text-sm">{artistName}</div>
         </div>
       </div>
       <HeartButton isFavorite={false} className="sm:mr-12" />
-    </div>
+    </li>
   )
 }
