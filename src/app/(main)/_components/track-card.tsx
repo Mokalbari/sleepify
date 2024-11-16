5d343854b67109048e2f495439522e085addc882
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-11-16 18:42:10 +0100

    refacto track card into reusable ui chunk

--- src/app/(main)/_components/track-card.tsx
+++ src/app/(main)/_components/track-card.tsx
@@ -1,5 +1,5 @@
 import CardPlayControl from "@/app/(main)/_components/card-play-control"
-import Image from "next/image"
+import TrackArtistBadge from "@/components/ui/track-artist-badge"
 
 interface Props {
   trackName: string
@@ -15,21 +15,11 @@ export default function TrackCard({
 }: Props) {
   return (
     <li className="brutal flex items-center justify-between rounded-md bg-white px-6 py-3 sm:py-3">
-      <div className="flex items-center">
-        <div className="relative w-12 sm:w-16">
-          <Image
-            src={trackImage}
-            alt={`Album cover of ${trackName}`}
-            width={640}
-            height={640}
-            style={{ borderRadius: "50%" }}
-          />
-        </div>
-        <div className="ml-4">
-          <h3 className="text-xs font-bold sm:text-base">{trackName}</h3>
-          <div className="text-2xs sm:text-xs">{artistName.join(", ")}</div>
-        </div>
-      </div>
+      <TrackArtistBadge
+        trackImage={trackImage}
+        trackName={trackName}
+        artistName={artistName}
+      />
       <CardPlayControl
         trackUrl={trackUrl}
         trackName={trackName}
