a0b29e0854569e1703520d9b272d512f1128aba3
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-11-16 19:53:34 +0100

    add table dumb component in responsive layout

--- src/app/likes/_components/table.tsx
+++ src/app/likes/_components/table.tsx
@@ -3,17 +3,18 @@ import TrackArtistBadge from "@/components/ui/track-artist-badge"
 import { cn } from "@/helpers/style"
 import { Play } from "lucide-react"
 import cover from "../../../../public/cover-album_placeholder.webp"
+import BadgeStack from "./badge-stack"
 
 export default function Table() {
   const fakeArray = ["Rh Factor, Nosia"]
 
   return (
-    <table className="brutal w-full bg-white">
+    <table className="w-full">
       <thead className="text-xs">
         <tr className="border-b border-b-black">
           <th className="p-2 text-left">Artist - Track</th>
-          <th className="max-lg:hidden">Counter</th>
-          <th className="p-2 text-right">Social - Play</th>
+          <th className="text-left max-lg:hidden">Smashed play button</th>
+          <th className="min-w-fit p-2 text-right">Social - Play</th>
         </tr>
       </thead>
       <tbody>
@@ -26,24 +27,19 @@ export default function Table() {
             />
           </td>
           <td className="max-lg:hidden">34 plays... You okay?</td>
-          <td className="p-2 text-right">
-            <div className="text-2xs">34 plays</div>
-            <div className="flex justify-end gap-4 sm:hidden">
-              <HeartButton isFavorite={false} className="w-4" />
-              <Play className={cn("w-4")} />
+          <td className="p-2 text-right lg:flex lg:h-full lg:items-center lg:justify-end lg:gap-8">
+            <div className="text-2xs lg:hidden">34 plays</div>
+            <div className="max-lg:hidden">
+              <BadgeStack />
+            </div>
+            <div className="flex justify-end gap-4">
+              <HeartButton
+                isFavorite={false}
+                className={cn("w-4 sm:w-5 lg:w-6")}
+              />
+              <Play className={cn("w-4 sm:w-5 lg:w-6")} />
             </div>
           </td>
-        </tr>
-        <tr className="">
-          <td>
-            <TrackArtistBadge
-              trackImage={cover}
-              artistName={fakeArray}
-              trackName="Common Freestyle"
-            />
-          </td>
-          <td className="max-lg:hidden">34 plays... You okay?</td>
-          <td>couscous</td>
         </tr>
       </tbody>
     </table>
