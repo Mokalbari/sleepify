699953d106f9dc14f746ef4ccaec1169f6e7e19f
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-11-16 18:26:08 +0100

    add header dumb comp to likes page

---
+++ src/app/likes/_components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <h2 className="text-md font-bold sm:text-lg lg:text-xl">Hi Juste!</h2>
+      <p className="text-2xs sm:text-xs lg:text-base">
+        Songs you’ve sworn your undying love to—until the next catchy beat
+        steals your attention.
+      </p>
+    </header>
+  )
+}
