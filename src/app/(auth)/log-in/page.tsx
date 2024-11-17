5084bd3a39c9e2969dbd0d85030bb9dc6f11bff2
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-11-17 11:10:41 +0100

    add lightweight form component to sign in as a user

--- src/app/(auth)/log-in/page.tsx
+++ src/app/(auth)/log-in/page.tsx
@@ -1,3 +1,29 @@
 export default function Page() {
-  return <div>login</div>
+  return (
+    <div className="grid min-h-screen place-content-center">
+      <form
+        action="#"
+        className="brutal flex flex-col gap-4 rounded-md bg-white p-4"
+      >
+        <div className="flex flex-col">
+          <label htmlFor="email">Email:</label>
+          <input
+            className="rounded-md border border-black"
+            type="email"
+            required
+          />
+        </div>
+        <div>
+          <label className="flex flex-col" htmlFor="password">
+            Password:
+          </label>
+          <input
+            className="rounded-md border border-black"
+            type="password"
+            required
+          />
+        </div>
+      </form>
+    </div>
+  )
 }
