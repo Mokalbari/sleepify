9b5d455cfd947ccd4ed23cc6aeccc23db0302bad
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-11-18 10:05:55 +0100

    add api endpoint to handle delete and post req

---
+++ src/app/(main)/likes/api/route.ts
@@ -0,0 +1,95 @@
+import { sql } from "@vercel/postgres"
+import { NextResponse } from "next/server"
+
+const USER_ID = "410544b2-4001-4271-9855-fec4b6a6442a"
+
+export async function DELETE(req: Request) {
+  try {
+    const { track_id } = await req.json()
+
+    if (!track_id) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: "A track ID must be provided to handle this request",
+        },
+        { status: 400 },
+      )
+    }
+
+    const result = await sql/*SQL*/ `
+  DELETE FROM favorites
+  WHERE user_id = ${USER_ID} and track_id = ${track_id}
+  `
+    if (result.rowCount === 0) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: "There was no favorite track to delete matching theses ID",
+        },
+        { status: 404 },
+      )
+    }
+
+    return NextResponse.json({ success: true }, { status: 200 })
+  } catch (error) {
+    let message = "Unknown Error"
+    if (error instanceof Error) message = error.message
+    else message = String(error)
+    return NextResponse.json(
+      { success: false, error: message },
+      { status: 500 },
+    )
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const { track_id } = await req.json()
+
+    if (!track_id) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: "The track id is required to create a new favorite track",
+        },
+        { status: 400 },
+      )
+    }
+
+    const { rows } = await sql/*sql*/ `
+    SELECT COUNT(*)::INTEGER FROM favorites
+    FROM favorites
+    WHERE user_id = ${USER_ID} AND track_id = ${track_id}`
+
+    const isAlreadyFavorite = rows[0]?.count > 0
+
+    if (isAlreadyFavorite) {
+      return NextResponse.json(
+        { success: false, message: "This track is already liked" },
+        { status: 400 },
+      )
+    }
+
+    const result = await sql/*sql*/ `
+    INSERT INTO favorites (user_id, track_id)
+    VALUES (${USER_ID}, ${track_id})
+    `
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: result.rows[0],
+      },
+      { status: 201 },
+    )
+  } catch (error) {
+    let message = "Unknown Error"
+    if (error instanceof Error) message = error.message
+    else message = String(error)
+    return NextResponse.json(
+      { success: false, error: message },
+      { status: 500 },
+    )
+  }
+}
