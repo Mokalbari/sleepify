fe9c1e47c2d0cf304854650148adce3c85755ae0
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-11-19 15:36:31 +0100

    refacto audio context into a separate hook useSleepifyPlayer

--- src/context/audio-context.tsx
+++ src/context/audio-context.tsx
@@ -1,34 +1,8 @@
 "use client"
-import {
-  createContext,
-  ReactNode,
-  useCallback,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react"
+import { useSleepifyPlayer } from "@/hooks/useSleepifyPlayer"
+import { createContext, ReactNode, useContext } from "react"
 
-export type AudioTrack = {
-  trackId: string
-  trackUrl: string | null
-  trackName: string
-  artistName: string[]
-  previewImage: string
-}
-
-type AudioContextType = {
-  isPlaying: boolean
-  currentTrack: AudioTrack | null
-  currentPlaylist: AudioTrack[]
-  currentTrackIndex: number
-  togglePlayPause: () => void
-  setAudioTrack: (track: AudioTrack) => void
-  skipNext: () => void
-  skipPrevious: () => void
-  setCurrentPlaylist: (playlist: AudioTrack[]) => void
-  setCurrentTrackIndex: (index: number) => void
-}
+type AudioContextType = ReturnType<typeof useSleepifyPlayer>
 
 type AudioProviderProps = {
   children: ReactNode
@@ -37,89 +11,15 @@ type AudioProviderProps = {
 const AudioContext = createContext<AudioContextType | undefined>(undefined)
 
 export default function AudioProvider({ children }: AudioProviderProps) {
-  const [currentTrack, setCurrentTrack] = useState<AudioTrack | null>(null)
-  const [currentPlaylist, setCurrentPlaylist] = useState<AudioTrack[]>([])
-  const [currentTrackIndex, setCurrentTrackIndex] = useState<number>(-1)
-  const [isPlaying, setIsPlaying] = useState(false)
-  const audioRef = useRef<HTMLAudioElement | null>(null)
-
-  const togglePlayPause = useCallback(() => {
-    if (!audioRef.current) return
-
-    if (isPlaying) {
-      audioRef.current.pause()
-    } else {
-      audioRef.current.play()
-    }
-    setIsPlaying(!isPlaying)
-  }, [isPlaying])
-
-  const setAudioTrack = useCallback((track: AudioTrack) => {
-    if (!audioRef.current || !track.trackUrl) return
-
-    setCurrentTrack(track)
-    audioRef.current.src = track.trackUrl
-    audioRef.current.play()
-    setIsPlaying(true)
-  }, [])
-
-  const skipNext = useCallback(() => {
-    if (!audioRef.current || currentPlaylist.length === 0) return
-
-    const nextIndex = (currentTrackIndex + 1) % currentPlaylist.length
-    const nextTrack = currentPlaylist[nextIndex]
-
-    setAudioTrack(nextTrack)
-    setCurrentTrackIndex(nextIndex)
-  }, [currentTrackIndex, currentPlaylist, setAudioTrack])
-
-  const skipPrevious = useCallback(() => {
-    if (!audioRef.current || currentPlaylist.length === 0) return
-
-    const prevIndex =
-      (currentTrackIndex - 1 + currentPlaylist.length) % currentPlaylist.length
-    const prevTrack = currentPlaylist[prevIndex]
-
-    setAudioTrack(prevTrack)
-    setCurrentTrackIndex(prevIndex)
-  }, [currentTrackIndex, currentPlaylist, setAudioTrack])
-
-  // Auto-play next track when current track ends
-  useEffect(() => {
-    const audioElement = audioRef.current
-
-    const handleEnded = () => {
-      skipNext()
-    }
-
-    if (audioElement) {
-      audioElement.addEventListener("ended", handleEnded)
-      return () => {
-        audioElement.removeEventListener("ended", handleEnded)
-      }
-    }
-  }, [skipNext])
+  const sleepifyPlayerProps = useSleepifyPlayer()
 
   return (
-    <AudioContext.Provider
-      value={{
-        isPlaying,
-        currentTrack,
-        currentPlaylist,
-        currentTrackIndex,
-        setCurrentPlaylist,
-        setCurrentTrackIndex,
-        togglePlayPause,
-        setAudioTrack,
-        skipNext,
-        skipPrevious,
-      }}
-    >
+    <AudioContext.Provider value={sleepifyPlayerProps}>
       {children}
       <audio
-        ref={audioRef}
-        onPlay={() => setIsPlaying(true)}
-        onPause={() => setIsPlaying(false)}
+        ref={sleepifyPlayerProps.audioRef}
+        onPlay={() => sleepifyPlayerProps.isPlaying}
+        onPause={() => sleepifyPlayerProps.isPlaying}
       />
     </AudioContext.Provider>
   )
