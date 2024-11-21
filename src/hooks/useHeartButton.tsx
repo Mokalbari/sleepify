e21c0c748042203272352edc43c0bc25d56bd5ae
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-11-21 17:21:57 +0100

    refacto heart button with accessibility and decoupled logic

---
+++ src/hooks/useHeartButton.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { useLikesContext } from "@/context/likes-context"
+import { useState } from "react"
+
+type Props = {
+  trackId: string
+  initialFavorite: boolean | undefined
+}
+
+export const useHeartButton = ({ trackId, initialFavorite }: Props) => {
+  const [isFavorite, setIsFavorite] = useState(initialFavorite)
+  const { incrementLikes, decrementLikes } = useLikesContext()
+
+  const toggleFavorite = async () => {
+    try {
+      if (isFavorite) {
+        const res = await fetch("/api/likes", {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ track_id: trackId }),
+        })
+
+        if (!res.ok) throw new Error("Failed to remove favorite")
+        decrementLikes()
+        setIsFavorite(false)
+      } else {
+        const res = await fetch("/api/likes", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ track_id: trackId }),
+        })
+
+        if (!res.ok) throw new Error("Failed to add a new favorite track")
+        incrementLikes()
+        setIsFavorite(true)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return { isFavorite, toggleFavorite }
+}
