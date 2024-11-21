44d4c0ee5025ae391211c091437918ffa052aca6
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-11-21 12:27:23 +0100

    add error handling for missing track in card component

--- src/app/(main)/_components/card-play-control.tsx
+++ src/app/(main)/_components/card-play-control.tsx
@@ -3,7 +3,9 @@
 import { usePlaylistContext } from "@/context/playlist-context"
 import { useSleepify } from "@/context/sleepify-context"
 import { TrackList } from "@/lib/types/definitions"
+import { cn } from "@/utils/helpers/style"
 import { Bug, Pause, Play } from "lucide-react"
+import { useState } from "react"
 import HeartButton from "../../../components/ui/heart-button"
 
 type Props = Pick<TrackList, "music_url" | "is_favorite" | "track_id">
@@ -13,28 +15,65 @@ export default function CardPlayControl({
   is_favorite: isFavorite,
   track_id: trackId,
 }: Props) {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
   const { currentTrack, isPlaying, playTrackFromPlaylist } = useSleepify()
-
   const { playlist } = usePlaylistContext()
+
+  const openPopover = () => {
+    setIsPopoverOpen(true)
+    setTimeout(() => {
+      setIsPopoverOpen(false)
+    }, 3000)
+  }
 
   return (
     <div className="flex gap-4">
       <HeartButton isFavorite={isFavorite} trackId={trackId} />
-      <button
-        onClick={() => playTrackFromPlaylist(trackId, trackUrl, playlist)}
-        disabled={!trackUrl}
-        className={`flex items-center justify-center ${
-          !trackUrl ? "cursor-not-allowed opacity-50" : ""
-        }`}
-      >
-        {trackUrl === null ? (
-          <Bug />
-        ) : isPlaying && currentTrack?.trackUrl === trackUrl ? (
-          <Pause />
-        ) : (
-          <Play />
-        )}
-      </button>
+
+      {trackUrl === null ? (
+        <div className="relative">
+          <button
+            className="flex cursor-help items-center justify-center opacity-50"
+            aria-label="Track unavailable"
+            onClick={openPopover}
+          >
+            <Bug />
+          </button>
+          {isPopoverOpen && (
+            <div
+              className={cn(
+                "brutal absolute bottom-10 right-0 z-10 w-80 rounded-md bg-deepBlue p-4 text-xs font-bold text-white transition-opacity duration-300",
+                "lg:text-base",
+                {
+                  "opacity-100": isPopoverOpen,
+                  "pointer-events-none opacity-0": !isPopoverOpen,
+                },
+              )}
+              role="tooltip"
+            >
+              Oops! Seems like this track is missing. We&apos;re on it!
+            </div>
+          )}
+        </div>
+      ) : (
+        <button
+          onClick={() => {
+            playTrackFromPlaylist(trackId, trackUrl, playlist)
+          }}
+          className="flex items-center justify-center"
+          aria-label={
+            isPlaying && currentTrack?.trackUrl === trackUrl
+              ? "Pause track"
+              : "Play track"
+          }
+        >
+          {isPlaying && currentTrack?.trackUrl === trackUrl ? (
+            <Pause />
+          ) : (
+            <Play />
+          )}
+        </button>
+      )}
     </div>
   )
 }
