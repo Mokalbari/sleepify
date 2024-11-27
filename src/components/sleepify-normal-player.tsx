88ad9a8bec1793865ee2f14a647c874aa30b06e8
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-11-27 14:25:27 +0100

    fix bottom nav not showing immediatly in mobile

--- src/components/sleepify-normal-player.tsx
+++ src/components/sleepify-normal-player.tsx
@@ -13,25 +13,27 @@ export default function SleepifyNormalPlayer() {
   const { handleFullPlayerVisibility } = useFullPlayer()
   const { currentTrack } = useSleepify()
   return (
-    <div
-      className={cn("sticky bottom-0 left-0 max-sm:mt-12", {
-        hidden: !currentTrack,
-        "animate-slide-up": currentTrack,
-      })}
-    >
+    <div className="sticky bottom-0 left-0">
       <div className="sm:hidden">
         <BottomNav />
       </div>
-      <ProgressBar />
-      <div className="flex w-full items-center justify-between bg-white px-4 py-3">
-        <PlayerTrack />
-        <button
-          onClick={handleFullPlayerVisibility}
-          className="font-bold text-accessBlue hover:cursor-pointer hover:underline max-sm:hidden"
-        >
-          <span className="max-lg:hidden">View</span> Lyrics
-        </button>
-        <PlayerControl />
+      <div
+        className={cn("", {
+          hidden: !currentTrack,
+          "animate-slide-up": currentTrack,
+        })}
+      >
+        <ProgressBar />
+        <div className="flex w-full items-center justify-between bg-white px-4 py-3">
+          <PlayerTrack />
+          <button
+            onClick={handleFullPlayerVisibility}
+            className="font-bold text-accessBlue hover:cursor-pointer hover:underline max-sm:hidden"
+          >
+            <span className="max-lg:hidden">View</span> Lyrics
+          </button>
+          <PlayerControl />
+        </div>
       </div>
     </div>
   )
